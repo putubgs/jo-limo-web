26ff7c5406fba5d7ef46beb55d86a971c857e088
SeedONova <putubaguswidia@gmail.com>
2025-07-26 08:52:01 +0700

    feat: contact us

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,44 @@
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+
+export default function Contact() {
+  return (
+    <>
+      <Header />
+      <section className="flex flex-col items-center justify-center">
+        <div className="flex flex-col items-center px-12 w-full max-w-[1000px] mx-auto gap-[90px] pt-[100px] pb-[170px] text-[20px]">
+          <h1 className="text-[64px]">Contact Information</h1>
+          <div className="flex flex-col gap-6 w-full">
+            <p className="text-[28px]">JO LIMO HEADQUARTER</p>
+            <p>
+              Shareef Jamil Bin Nasser Street, King Abdullah Gardens, PO Box
+              961003, Amman 11196, Jordan.
+            </p>
+          </div>
+          <div className="flex flex-col gap-6 w-full">
+            <p className="text-[28px]">CONTACT US</p>
+            <div className="flex gap-8 justify-between">
+              <div className="flex flex-col text-[20px]">
+                <p>Customer Service</p>
+                <a href="mailto:support@jo-limo.com" className="underline">support@jo-limo.com</a>
+              </div>
+              <div className="flex flex-col text-[20px]">
+                <p>Corporate B2B</p>
+                <a href="mailto:b2b@jolimo.com" className="underline">b2b@jolimo.com</a>
+              </div>
+              <div className="flex flex-col text-[20px]">
+                <p>Bookings</p>
+                <a href="mailto:booking@jolimo.com" className="underline">booking@jolimo.com</a>
+              </div>
+              <div className="flex flex-col text-[20px]">
+                <p>Mobile</p>
+                <p className="underline">+962 7962 27227</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+}
