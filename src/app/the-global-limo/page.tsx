e34ced8b6266ec44016673811c46fcebdbebf950
SeedONova <putubaguswidia@gmail.com>
2025-08-24 18:54:04 +0700

    fix: global limo text

--- src/app/the-global-limo/page.tsx
+++ src/app/the-global-limo/page.tsx
@@ -7,7 +7,7 @@ export default function AboutUs() {
     <>
       <Header />
       <section className="flex flex-col items-center justify-center text-center mt-[50px] mb-[100px] max-w-[1000px] mx-auto gap-[50px]">
-        <h1 className="text-[32px]">International Chauffeur Charter with AirportConnect</h1>
+        <h1 className="text-[42px]">International Chauffeur Charter with AirportConnect</h1>
         <Image
           src="/images/global_limo.png"
           alt="The Global Limo Image"
