b995853eea9c9961d381a6bed47789fbb7fe73c4
SeedONova <putubaguswidia@gmail.com>
2025-09-30 19:18:54 +0700

    feat: corporate mobility login mobile design

--- src/app/corporate-mobility/login/page.tsx
+++ src/app/corporate-mobility/login/page.tsx
@@ -57,12 +57,12 @@ export default function LoginCorporateMobility() {
     <>
       <Header />
       <section className="flex items-center justify-center py-[150px]">
-        <div className="flex flex-col items-center max-w-[700px] w-full gap-2 text-center">
-          <p className="text-[36px] w-full">
+        <div className="flex flex-col items-center md:max-w-[700px] px-5 md:px-0 w-full gap-2 text-center">
+          <p className="text-[36px] w-full leading-tight">
             Login to Your Corporate Mobility Account
           </p>
           <div className="flex flex-col items-center max-w-[587px] w-full gap-8 text-center">
-            <p className="text-[16px]">
+            <p className="md:text-[16px] text-[14px]">
               View and track your bookings and billing information with ease.
             </p>
 
@@ -104,7 +104,7 @@ export default function LoginCorporateMobility() {
                 {loading ? "Signing In..." : "Continue"}
               </button>
             </form>
-            <p className="text-center text-black px-10 text-[#656565]">
+            <p className="text-center text-[14px] md:text-[16px] text-black px-10 text-[#656565]">
               Not a Jo Limo partner yet? Partner with us and elevate your
               business mobility.{" "}
               <Link href={"#"} className="underline text-blue-500">
