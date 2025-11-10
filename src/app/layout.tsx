e209f0008ff2598adccafb35f76dd11348a61e25
SeedONova <putubaguswidia@gmail.com>
2025-11-10 18:55:09 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import ConditionalLayout from "@/app/ConditionalLayout";
+import Script from "next/script";
 import "./globals.css";
 
 const plusJakartaSans = Plus_Jakarta_Sans({
@@ -24,6 +25,22 @@ export default function RootLayout({
       lang="en"
       className={`flex items-center justify-center ${plusJakartaSans.variable}`}
     >
+      <head>
+        {/* Global site tag (gtag.js) - Google Ads */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17646839146"
+        />
+
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17646839146');
+          `}
+        </Script>
+      </head>
       <body className="w-full">
         <ConditionalLayout>{children}</ConditionalLayout>
       </body>
