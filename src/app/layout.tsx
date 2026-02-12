0be09a476a40a41c63dc4209e1aeb804c7efe48c
SeedONova <putubaguswidia@gmail.com>
2026-02-12 17:17:12 +0700

    feat: adding google tag manager script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
       lang="en"
       className={`flex items-center justify-center ${plusJakartaSans.variable}`}
     >
-      <head>
-        {/* Global site tag (gtag.js) - Google Ads */}
+      {/* Google site tag (gtag.js) - Google Ads */}
+      {/* <head>
         <Script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-17646839146"
@@ -41,8 +41,35 @@ export default function RootLayout({
             gtag('config', 'AW-17646839146');
           `}
         </Script>
+      </head> */}
+
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NW2HGKDC');
+            `,
+          }}
+        />
       </head>
+
       <body className="w-full">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NW2HGKDC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <StoreHydration />
         <ConditionalLayout>{children}</ConditionalLayout>
       </body>
