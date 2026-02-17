e11ae39140585ca5bafc5fd811af634254e75477
SeedONova <putubaguswidia@gmail.com>
2026-02-18 02:54:14 +0800

    update prisma ts and schema

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,22 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString){
+    throw new Error("DATABASE_URL is not defined");
+}
+
+const adapter = new PrismaPg({ connectionString });
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
 };
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["error"],
+    adapter
   });
 
 if (process.env.NODE_ENV !== "production") {
